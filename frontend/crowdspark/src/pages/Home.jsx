8c58bb83b04c7a15fffc24ec366af4522f0e21ef
Ansh240204 <ansh10raghu@gmail.com>
2025-06-20 20:06:51 +0530

    Commit done

---
+++ frontend/crowdspark/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="p-10 text-center text-xl">
+      Welcome to CrowdSpark!
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/crowdspark/src/pages/HowItWorks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HowItWorks = () => {
+  return (
+    <div className="p-10 text-xl text-center">
+      how it works
+      
+    </div>
+  )
+}
+
+export default HowItWorks
+
