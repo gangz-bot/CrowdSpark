d1e0be9a589a2b28511c8562fb5ad119829baa2a
Ashutosh110165 <dashutosh466@gmail.com>
2025-06-23 09:20:38 +0530

    push

---
+++ src/pages/RealTimeUpdates.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RealTimeUpdates = () => {
+  return (
+    <div style={{ padding: '3rem 2rem', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '2.5rem', color: '#014f56' }}>Real-Time Updates</h1>
+      <p style={{ fontSize: '1.2rem', marginTop: '1rem' }}>
+        Track projects and donations easily.
+      </p>
+    </div>
+  );
+};
+
+export default RealTimeUpdates;
